b835d7f77b73e034511af2066b9dcf7e78853d12
jungj0925 <jungj0925@gmail.com>
2024-12-29 02:24:41 -0500

    added debugging code

--- server.js
+++ server.js
@@ -49,6 +49,7 @@ async function refreshAccessToken() {
 
 async function makeSpotifyRequest(url) {
     try {
+        console.log(process.env.SPOTIFY_ACCESS_TOKEN);
         const response = await axios.get(url, {
             headers: {
                 'Authorization': `Bearer ${process.env.SPOTIFY_ACCESS_TOKEN}`
